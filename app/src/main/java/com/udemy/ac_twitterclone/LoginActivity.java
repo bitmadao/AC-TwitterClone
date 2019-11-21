215b58ddf0c176fa4d778fdfe4b700107d739658
bitmadao <dag@pepmagic.no>
2019-11-21 15:39:31 +0100

    add cartoon_bluebird.png for use as logo. create layout for LoginActivity with empty methods for functionality

--- app/src/main/java/com/udemy/ac_twitterclone/LoginActivity.java
+++ app/src/main/java/com/udemy/ac_twitterclone/LoginActivity.java
@@ -1,18 +1,50 @@
 package com.udemy.ac_twitterclone;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.parse.ParseInstallation;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnLogin, btnNeedAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        btnLogin = findViewById(R.id.btnLoginActivityLogin);
+        btnNeedAccount = findViewById(R.id.btnLoginActivityLoginNeedAccount);
+
+        btnLogin.setOnClickListener(LoginActivity.this);
+        btnNeedAccount.setOnClickListener(LoginActivity.this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLoginActivityLogin:
+                btnLoginTapped();
+                break;
+
+            case R.id.btnLoginActivityLoginNeedAccount:
+                btnNeedAccountTapped();
+                break;
+        }
+    }
+
+    private void btnLoginTapped() {
+
+    }
+
+    private void btnNeedAccountTapped() {
+
     }
 }
