a9619b8e0a86840cb66d88f7f67987c1174d63be
bitmadao <dag@pepmagic.no>
2019-11-27 19:26:39 +0100

    Initial commit of TroughActivity. it is not a feed, it's a trough

---
+++ app/src/main/java/com/udemy/ac_twitterclone/TroughActivity.java
@@ -0,0 +1,14 @@
+package com.udemy.ac_twitterclone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TroughActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trough);
+    }
+}
